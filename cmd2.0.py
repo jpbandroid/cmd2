80c32caa5ff13ca732712b2a282006076bcbcf2f
jpb <jakub.baranski.33@gmail.com>
2023-05-18 07:03:57 +0100

    Beta 3, reliability improvements

--- cmd2.0.py
+++ cmd2.0.py
@@ -2,6 +2,8 @@ import math
 import os
 from os.path import exists as file_exists
 import shutil
+from datetime import datetime
+import platform
 
 def mkdir(*name):
     dirname = ' '.join(name)
@@ -11,8 +13,10 @@ def rmdir(*name):
     shutil.rmtree(path)
 def delete(*name):
     path = ' '.join(name)
-    print(path)
     os.remove(path)
+def time_get():
+    time_now = datetime.now()
+    print(time_now)
 def dir(path = None):
     if path == "":
         cwd = os.getcwd()
@@ -29,28 +33,44 @@ def cd(path):
     os.chdir(path)
 def run(path):
     os.system(path)
+def help():
+    print('''All the available commands in cmd2 Beta 3:
+cd - Opens a directory to be viewed in cmd2
+curdate - gets the current date & time
+del - delete a file from the current directory
+dir - display what files and directories are inside a certain directory
+exit - exits cmd2
+mkdir - create a directory inside the current directory
+pi - displays π
+rmdir - removes a directory inside the current directory
+<name of exe file> - runs an executable with the name you have inputted
+help - shows you descriptions of all commands in cmd2''')
 def exit(*args):
     quit()
+def ver():
+    print("cmd2 Version 1.0.0.3 (Beta 3)\nCopyright © 2023 jpb\nLicensed under MIT License\n\n" + platform.system(), platform.release(), platform.version())
 
 
-functions = {'del': delete, 'rmdir': rmdir, '': run, 'cd': cd, 'echo': echo, 'dir': dir, 'mkdir': mkdir, 'pi': pi, 'exit': exit}  # The available functions.  
+functions = {'ver': ver, 'help': help,'curdate': time_get, 'del': delete, 'rmdir': rmdir, '': run, 'cd': cd, 'echo': echo, 'dir': dir, 'mkdir': mkdir, 'pi': pi, 'exit': exit}  # The available functions.  
 
-print('''cmd2 [Version 1.0 BETA 2]
+print('''cmd2 [Version 1.0 BETA 3]
 © 2023 jpb''')
 while True:
     cwd = os.getcwd()
     command, *arguments = input(cwd + "> ").strip().split(' ')  # Take the user's input and split on spaces.
     isExist = file_exists(command)
-    if isExist == True:
-        run(command)
-    else:
-        run("C:\\Windows\\" + command)
-        isExist2 = file_exists("C:\\Windows\\" + command)
-        if isExist2 == False:
-            run("C:\\Windows\\System32\\" + command)
-            isExist3 = file_exists("C:\\Windows\\System32\\" + command)
-            if isExist3 == False:
-                if command not in functions:
+    if command not in functions:
+        if '.exe' not in command:
+            command = command + '.exe'
+        if isExist == True:
+            run(command)
+        else:
+            run("C:\\Windows\\" + command)
+            isExist2 = file_exists("C:\\Windows\\" + command)
+            if isExist2 == False:
+                run("C:\\Windows\\System32\\" + command)
+                isExist3 = file_exists("C:\\Windows\\System32\\" + command)
+                if isExist3 == False:
                     print("Couldn't find command", command)
-                else:
-                    functions[command](*arguments)
+    else:
+        functions[command](*arguments)
