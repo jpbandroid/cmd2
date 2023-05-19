1176209404eaa99531e5dc29bc8811b89cf73f4b
jpb <jakub.baranski.33@gmail.com>
2023-05-19 06:34:04 +0100

    cmd2 v1, many improvements

--- cmd2.0.py
+++ cmd2.0.py
@@ -5,9 +5,34 @@ import shutil
 from datetime import datetime
 import platform
 
+def calc(num1, operator, num2):
+    if num1 == "pi":
+        num1 = float(math.pi)
+        if num2 == "pi":
+            num2 = float(math.pi)
+        else:
+            num2 = int(num2)
+    elif num2 == "pi":
+        num1 = int(num1)
+        num2 = float(math.pi)
+    else:
+        num1 = int(num1)
+        num2 = int(num2)
+    if operator == "+":
+        print("Result: " + str(num1 + num2))
+    elif operator == "-":
+        print("Result: " + str(num1 - num2))
+    elif operator == "*":
+        print("Result: " + str(num1 * num2))
+    elif operator == "/":
+        print("Result: " + str(num1 / num2))
+    else:
+        print("Invalid operator has been provided. Please retry by typing the 'calc' command again.")
 def mkdir(*name):
     dirname = ' '.join(name)
     os.makedirs(dirname)
+def rename(filename, newname):
+    os.rename(filename, newname)
 def rmdir(*name):
     path = ' '.join(name)
     shutil.rmtree(path)
@@ -34,7 +59,8 @@ def cd(path):
 def run(path):
     os.system(path)
 def help():
-    print('''All the available commands in cmd2 Beta 3:
+    print('''All the available commands in cmd2 v1:
+calc - calculator in your terminal!
 cd - Opens a directory to be viewed in cmd2
 curdate - gets the current date & time
 del - delete a file from the current directory
@@ -43,17 +69,18 @@ exit - exits cmd2
 mkdir - create a directory inside the current directory
 pi - displays π
 rmdir - removes a directory inside the current directory
-<name of exe file> - runs an executable with the name you have inputted
-help - shows you descriptions of all commands in cmd2''')
+<name of exe file> - runs an executable with the name you have inserted
+help - shows you descriptions of all commands in cmd2
+ren (or remane) - allows you to rename a file''')
 def exit(*args):
     quit()
 def ver():
-    print("cmd2 Version 1.0.0.3 (Beta 3)\nCopyright © 2023 jpb\nLicensed under MIT License\n\n" + platform.system(), platform.release(), platform.version())
+    print("cmd2 Version 1.0.0 (Stable v1)\nCopyright © 2023 jpb\nLicensed under MIT License\n\n" + platform.system(), platform.release(), platform.version())
 
 
-functions = {'ver': ver, 'help': help,'curdate': time_get, 'del': delete, 'rmdir': rmdir, '': run, 'cd': cd, 'echo': echo, 'dir': dir, 'mkdir': mkdir, 'pi': pi, 'exit': exit}  # The available functions.  
+functions = {'rename': rename, 'ren': rename, 'calc': calc, 'ver': ver, 'help': help,'curdate': time_get, 'del': delete, 'rmdir': rmdir, '': run, 'cd': cd, 'echo': echo, 'dir': dir, 'mkdir': mkdir, 'pi': pi, 'exit': exit}  # The available functions.  
 
-print('''cmd2 [Version 1.0 BETA 3]
+print('''cmd2 [Version 1.0, built 19/5/2023]
 © 2023 jpb''')
 while True:
     cwd = os.getcwd()
